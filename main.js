056447dcdba1cda16084bc88255b98fe415f54ec
Justin Bigishiro <justin.rvd@gmail.com>
2023-05-05 12:04:47 -0500

    adds js code to POST seller data on json file

--- main.js
+++ main.js
@@ -1,35 +1,62 @@
-function sellerPage(seller){
-    //build animal
-    let card=document.createElement("li")
-    card.className="card"
-    card.style.textAlign="center"
-    card.innerHTML=`
-     <img src=${seller.imageUrl} width="400" height="300">
+//create an eventListenerr to submit the seller form
+document.getElementById("form1").addEventListener("submit", submitSellerForm);
+// function to submit a form
+function submitSellerForm(e) {
+  e.preventDefault();
+  let cowObj = {
+    name: e.target.name.value,
+    phoneNumber: e.target.phoneNumber.value,
+    address: e.target.address.value,
+    imageUrl: e.target.imageUrl.value,
+    price: e.target.price.value,
+    weight: e.target.weight.value,
+  };
+  sellerPage(cowObj);
+  addCow(cowObj);
+}
+// function to create the listing page
+function sellerPage(seller) {
+  let card = document.createElement("div");
+  card.id = "card";
+  card.style.textAlign = "left";
+  let cow = document.createElement("div");
+  cow.id = "cow";
+  let sellerInfo = document.createElement("div");
+  sellerInfo.id = "sellerInfo";
+  cow.innerHTML = `<img src=${seller.imageUrl} width="400" height="300">`;
+  sellerInfo.innerHTML = `
      <div class="content">
-      <p>Name: ${seller.name}</p><br>
-      <p>Phone Number: ${seller.phoneNumber}</p><br>
-      <p>${seller.address}</p><br>
-      <p>${seller.price}</p><br>
-      <p>${seller.weight}</p><br>
+      <p> Seller Name: ${seller.name}</p>
+      <p>Phone Number: ${seller.phoneNumber}</p>
+      <p>Address: ${seller.address}</p>
+      <p>Price: ${seller.price}</p>
+      <p>Weight: ${seller.weight}</p>
       </div>
-    `
-    //add animal card to DOM
-document.getElementById("sellerListing").appendChild(card)
-    
+    `;
+  card.appendChild(cow);
+  card.appendChild(sellerInfo);
+  //add  card to DOM
+  document.getElementById("sellerListing").appendChild(card);
 }
 // fetch requests
-//Get Fetch for all animal resources
-function getSellerData(){
-    fetch('http://localhost:3000/sellerData')
-    .then(res=>res.json())
-  .then(sellerData=>sellerData.forEach(seller=>sellerPage(seller)))
-
+//Get Fetch for the db.json file resources
+function getSellerData() {
+  fetch("http://localhost:3000/sellerData")
+    .then((res) => res.json())
+    .then((sellerData) => sellerData.forEach((seller) => sellerPage(seller)));
 }
 
-
-// initial render
-// get data and render our animals to the DOM
-function initialize(){
-  getSellerData()
+function addCow(cowObj) {
+  fetch("http://localhost:3000/sellerData", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cowObj),
+  }).then((resp) => resp.json());
 }
-initialize()
+
+function initialize() {
+  getSellerData();
+}
+initialize();
